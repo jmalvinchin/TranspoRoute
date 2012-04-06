fd0eba6a3dd930c0b3b3059910f230f22d37c42b
Jess Malvin Chin <jess@teamcodeflux.com>
2012-04-06 15:12:13 +0800

    Initial rails setup
    Signed-off-by: Jess Malvin Chin <jess@teamcodeflux.com>

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TranspoRoute::Application.config.session_store :cookie_store, key: '_TranspoRoute_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TranspoRoute::Application.config.session_store :active_record_store
